4efa2f3e671384d695bd7042d0e61e2242446f60
zhanc2 <76535472+zhanc2@users.noreply.github.com>
2022-01-22 12:01:40 -0500

    Revert "Delete 2006_S3_Tin_Can_Telephone.h"
    This reverts commit a7464e9516b22b70d496ae68b0dc1efef7ef22b3.

---
+++ CCC_Problems/2006_S3_Tin_Can_Telephone.h
@@ -0,0 +1,9 @@
+//
+// Created by Charles Zhang on 2021-12-28.
+//
+//https://dmoj.ca/problem/ccc06s3
+
+#ifndef CONTEST_PROBLEMS_2006_S3_TIN_CAN_TELEPHONE_H
+#define CONTEST_PROBLEMS_2006_S3_TIN_CAN_TELEPHONE_H
+
+#endif //CONTEST_PROBLEMS_2006_S3_TIN_CAN_TELEPHONE_H
